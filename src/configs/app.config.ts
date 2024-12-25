1d7e3abbe03898b10df1d38347da9d94e2540f1a
HabibovU <habibovulugbek22@gmail.com>
2024-12-25 14:23:22 +0500

    port env

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -9,5 +9,5 @@ export const appConfig: AppConfig = {
   env: process.env.NODE_ENV ?? undefined,
   name: process.env.APP_NAME ?? undefined,
   host: process.env.APP_HOST ?? '0.0.0.0',
-  port: process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 4000,
+  port: process.env.PORT ? parseInt(process.env.PORT, 10) : 4000,
 }
